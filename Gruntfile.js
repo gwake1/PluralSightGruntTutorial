fd7581d9f90f35c036702eaa7a1c7aa9dfbd7d50
Gerald A. Wakefield <gwake1@gmail.com>
2015-02-18 20:57:38 -0600

    executed grunt default -v

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,5 +7,5 @@ module.exports = function(grunt){
     }
   });
   grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.registerTasks("default", ['clean']);
+  grunt.registerTask("default", ['clean']);
 }
